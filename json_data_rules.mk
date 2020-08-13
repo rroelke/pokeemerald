ce7e52d308105f24ce0d3670131457ccc63041fc
Ryan Roelke <rroelke0@gmail.com>
2020-08-12 23:11:24 -0400

    Generate trainer source code from data/trainers.json

--- json_data_rules.mk
+++ json_data_rules.mk
@@ -1,10 +1,24 @@
 # JSON files are run through jsonproc, which is a tool that converts JSON data to an output file
 # based on an Inja template. https://github.com/pantor/inja
 
+
 $(DATA_SRC_SUBDIR)/wild_encounters.h: $(DATA_SRC_SUBDIR)/wild_encounters.json $(DATA_SRC_SUBDIR)/wild_encounters.json.txt
 	$(JSONPROC) $^ $@
 
 $(C_BUILDDIR)/wild_encounter.o: c_dep += $(DATA_SRC_SUBDIR)/wild_encounters.h
 
 
-AUTO_GEN_HEADERS += $(DATA_SRC_SUBDIR)/wild_encounters.h
+TRAINERS_JSON := data/trainers.json
+
+TRAINER_HEADERS :=                   \
+    include/constants/opponents.h    \
+    src/data/trainers.h              \
+    src/data/trainer_parties.h
+
+$(TRAINER_HEADERS): %.h: $(TRAINERS_JSON) %.h.template
+	$(JSONPROC) $^ $@
+
+trainers_templates: $(TRAINER_HEADERS)
+
+
+AUTO_GEN_HEADERS += $(DATA_SRC_SUBDIR)/wild_encounters.h $(TRAINER_HEADERS)
